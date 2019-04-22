b4b2489144428686057b02da36b4af601eb36e53
briandolan1985 <46466179+briandolan1985@users.noreply.github.com>
2019-04-22 19:40:29 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+function hideit() {
+  document.getElementById("hawaii").className = "makeInvisible";
+}
+
+funtion changeText() {
+  document.getElementById("text").style.fontSize = "5em";
+  document.getElementById("text").style.fontStyle = "Italiac";
+  document.getElementById("text").style.backgroundColor = "red";
+  document.getElementById("text").style.fontWeight = "Bold";
+}
+
+  function refresh() {
+    location.reload();
+  }
+
+  function alterText(){
+    //This is for the 5.html file
+    document.getElementById("schools").style.fontSize = "5em";
+    document.getElementById("schools").style.fontStyle = "Italiac";
+    document.getElementById("schools").style.backgroundColor = "yellow";
+    document.getElementById("schools").style.fontWeight = "Bold";
+  }
